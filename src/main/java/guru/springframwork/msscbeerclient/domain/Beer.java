fce6ca3128bfcd21f0eb73ef8faec3db9fe00649
Jan Oleksik <jan.oleksik@hycom.pl>
2022-08-31 18:15:33 +0200

    domain

---
+++ src/main/java/guru/springframwork/msscbeerclient/domain/Beer.java
@@ -0,0 +1,22 @@
+package guru.springframwork.msscbeerclient.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+/**
+ * @author Jan Oleksik (jan.oleksik@hycom.pl)
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Beer {
+    private UUID id;
+    private String beerName;
+    private BeerStyleEnum beerStyle;
+    private Long upc;
+}
